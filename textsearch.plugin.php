ededb01a9ecad11564e28ab4964fcdcd2b998b1a
John Edmondson <mumbles@gmail.com>
2014-02-14 17:18:38 +0000

    Update textsearch.plugin.php
    new chnages

--- textsearch.plugin.php
+++ textsearch.plugin.php
@@ -23,63 +23,22 @@ class textsearch extends Plugin
 
 	/**
 	 * function save_price
-	 * Determines whether a thumbnail needs to be created for this post, and adds it to the postinfo for this post
-	 * @param Post the post for which the thumb should be generated
+
+	 * @param Post the post for which the price should be stored.
 	**/
 	public function save_price( $post )
 	{
-		// set up a temporary variable to capture the image tag(s) �\d{4} should do it
- 
-		$matches= array();
-		if ( preg_match( '£\d{1,4}', $post->content, $matches) ) {
-			// we got one! Now tease out the src element
-			$html= new HTMLTokenizer( $matches[0] );
-/* need to check what's in $html. Should be something like £1234 */
+		// set up a temporary variable to capture the first price found
+		$matches = array();
+		if ( preg_match( '/£[\d,.]*\d/', $post->content, $matches) ) {
 
-			$tokens= $html->parse();
-			foreach ($tokens as $node ) {
-				if ( 'img' == $node['name'] ) {
-					$elements= $node['attrs'];
-				}
-			}
-		}
-		//if ( ! isset( $elements['src'] ) ) {
-			//// no src= found, so don't try to do anything else
-		//	return;
-		//}
+			$price = $matches[0];
 
-		$thumb= $post->info->price;
+		$thumb = $post->info->price;
 
-		if ( ! isset( $thumb ) ) {
-			// no thumbnail exists for this post yet, so make one
-			$post->info->textsearch_thumb= $this->make_thumbnail( $elements['src'] );
-			$post->info->textsearch_md5= md5_file( $this->get_image_file( $elements['src'] ) );
-			$post->info->commit();
-		} else {
-			// a thumbnail exists; we should check whether we need to update it
-			if (true) { // ( md5_file( $this->get_image_file( $elements['src'] ) ) != $post->info->textsearch_md5 ) {
-				// the image has a different MD5 sum than the
-				// one we previously calculated for it, so
-				// generate a new thumbnail
-				$post->info->textsearch_thumb= $this->make_thumbnail( $elements['src'] );
-				$post->info->textsearch_md5= md5_file( $this->get_image_file( $elements['src'] ) );
-				$post->info->commit();
-			}
+		$post->info->price = $price;
+		$post->info->commit();
 		}
 	}
-
-	/**
-	 * post_filter_content_excerpt_out
-	 * filters the post's excerpt to display only the thumbnail
-	**/
-	public function filter_post_content_excerpt_out ( $excerpt, $post )
-	{
-		if (isset( $post->info->textsearch_thumb ) ) {
-			return $post->info->textsearch_thumb;
-		} else {
-			return $excerpt;
-		}
-	}	
 }
 ?>
-
